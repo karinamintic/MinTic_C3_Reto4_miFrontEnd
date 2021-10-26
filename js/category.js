678dbdf4909d271cd66c8f17ef82fe4f72efa5ea
karinamintic <karina.arias.mt@correo.usa.edu.co>
2021-10-25 22:05:24 -0500

    Crud Categoria Funcionando
    Ya complete el Crud de Categoria

--- js/category.js
+++ js/category.js
@@ -63,9 +63,14 @@ function guardarInformacion()
             ///Codigo para actualizar tabla sin actualizar toda la página
             $('#tablaCategory').dataTable().fnDestroy();
             traerInformacion();
+            ///Toast
+            M.toast({html: 'Felicidades!!! ha creado una categoria'});
             ///
             ////Cerrar Modal
             $('#modalGuardar').modal('close');
+            ///Limpiar inputs
+            $("#name").val('');
+            $("#description").val('');
 
         },
         error : function(e) {
@@ -77,11 +82,6 @@ function guardarInformacion()
     });
 }
 
-/* $(document).on("click", ".btnBorrar", function(){
-    //alert(recuperarJson[0].id);
-    var table = $('#tablaCategory').DataTable();
-    var miIndice;
- }); */
 
 /////Función para capturar el indice del dataTable
  $('#tablaCategory tbody').on( 'click', 'tr', function ()
@@ -91,7 +91,11 @@ function guardarInformacion()
         //alert( 'Row index: '+table.row( this ).index() );
         //alert(miIndice)
         miIndice = recuperarJson[miIndice].id;
-        borrarCategoria(miIndice); ////Llamo la función con el id Capturado
+
+});
+$(document).on("click", ".btnBorrar", function(){
+
+    borrarCategoria(miIndice);
 });
 
 function borrarCategoria(idElemento){
@@ -110,72 +114,48 @@ function borrarCategoria(idElemento){
             ///Codigo para actualizar tabla sin actualizar toda la página
             $('#tablaCategory').dataTable().fnDestroy();
             traerInformacion();
+            ////
+            M.toast({html: 'Ha borrado la categoria'});
         }
     });
 
 }
-
-/* ///////Boton borrar
-$(document).on("click", ".btnBorrar", function(){
-    fila = $(this);           
-    user_id = parseInt($(this).closest('tr').find('td:eq(0)').text()) ;		
-    opcion = 3; //eliminar        
-    var respuesta = confirm("¿Está seguro de borrar el registro "+user_id+"?");
-    let myData = {
-        id:user_id
-    }
-    let dataToSend=JSON.stringify(myData);                
-    if (respuesta) {            
-        $.ajax({
-          url: "https://g4ef86a70a56228-db202109252211.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/skate/skate",
-          type: "DELETE",  
-          data: dataToSend,
-          contentType: "application/JSON", 
-          datatype:"JSON",    
-          success: function() {
-                location.reload();                 
-           }
-        });	
-    }
- });
-//////Boton Editar
- $(document).on("click", ".btnEditar", function(){		        
+/////////// EDITAR
+/////Recoger datos para el modal
+$(document).on("click", ".btnEditar", function(){
     opcion = 2;//editar
-    fila = $(this).closest("tr");	        
-    v_id = parseInt(fila.find('td:eq(0)').text()); //capturo el ID
-    var v_name = fila.find('td:eq(1)').text();
-    var v_brand = fila.find('td:eq(2)').text();
-    var v_model = fila.find('td:eq(3)').text();
-    var v_category = parseInt(fila.find('td:eq(4)').text());
+    fila = $(this).closest("tr");
+    var v_name = fila.find('td:eq(0)').text();
+    var v_descripcion = fila.find('td:eq(1)').text();
 
-    $("#id_e").attr("disabled","disabled");
-    
-    $("#id_e").val(v_id);
     $("#name_e").val(v_name);
-    $("#brand_e").val(v_brand);
-    $("#model_e").val(v_model);
-    $("#category_id_e").val(v_category);
-    $('#modal2').modal('open');		   
+    $("#description_e").val(v_descripcion);
+    $('#modalEditar').modal('open');
+
 });
 
 function editarInformacion()
 {
     let myData = {
-        id:$("#id_e").val(),
+        id: miIndice, ////desde la función #tablaCategory lo capturo
         name:$("#name_e").val(),
-        brand:$("#brand_e").val(),
-        model:$("#model_e").val(),
-        category_id:$("#category_id_e").val(), 
+        description:$("#description_e").val(),
     }
     let dataToSend=JSON.stringify(myData);
     $.ajax({
-        url:"https://g4ef86a70a56228-db202109252211.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/skate/skate",
+        url:"http://150.230.95.72:8080/api/Category/update",
         type: "PUT",
         data: dataToSend,
         contentType: "application/JSON",
         datatype:"JSON",
-        success:function(respuesta){   
-            location.reload();
+        success:function(respuesta){
+            ///Codigo para actualizar tabla sin actualizar toda la página
+            $('#tablaCategory').dataTable().fnDestroy();
+            traerInformacion();
+            ////
+            $('#modalEditar').modal('close');
+            M.toast({html: 'Ha editado la categoria'});
         }
     });
-} */
+}
+
